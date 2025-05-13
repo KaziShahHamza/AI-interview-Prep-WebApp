fcd26b2d923017ac2fe82af1ef9457b99ae2c8ec
soulfulDreamer <kazishahhamza@outlook.com>
2025-05-14 04:02:38 +0600

    Add auth funcs

---
+++ server/controllers/authController.js
@@ -0,0 +1,84 @@
+import User from "../model/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// Generates a JSON Web Token for a user.
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
+
+// POST, /api/auth/register, Register User
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, profileImageUrl } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.json(400).json({ message: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      profileImageUrl,
+    });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+// POST, /api/auth/login, Login User
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(500).json({ message: "User doesn't exists." });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(500).json({ message: "Password doesn't match." });
+    }
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "LoginUser Catched Error." });
+  }
+};
+
+// GET, /api/auth/profile, (privete), Get User Profile
+const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) {
+      res.status(500).json({ message: "User doesn't exists" });
+    }
+    res.json(user);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error while getting user", error: error.message });
+  }
+};
+
+export { registerUser, loginUser, getUserProfile };
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import path from "path";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -19,12 +20,22 @@ app.use(
   })
 );
 
+// Database Connection
 connectDB();
 
 app.use(express.json());
 
+// Routes
+app.use("/api/auth", authRoutes);
+// app.use("/api/session", sessionRoutes);
+// app.use("/api/question", questionRoutes);
+
+// app.use("/api/generate-questions", protect, generateQuestions);
+// app.use("/api/generate-explanation", protect, generateExplanation);
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use("/upload", express.static(path.join(__dirname, "uploads"), {}));
 
+// Server Start
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
